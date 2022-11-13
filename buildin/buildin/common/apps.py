7738c4f1c924a23f8ba9473e68c6222f967fad2d
TanyaAng <t.j.angelova@gmail.com>
2022-11-13 20:03:45 +0200

    Added signals to TaskComment model

--- buildin/buildin/common/apps.py
+++ buildin/buildin/common/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CommonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'buildin.common'
+
+    def ready(self):
+        from . import signals
--- buildin/buildin/common/signals.py
+++ buildin/buildin/common/signals.py
@@ -1,1 +1,9 @@
+from django.db.models import signals
+from django.dispatch import receiver
 
+from buildin.common.models import TaskComment
+
+
+@receiver(signals.post_save, sender=TaskComment)
+def comment_created(instance, **kwargs):
+    print(f"{instance.user} added new comment to {instance.to_task}.")
